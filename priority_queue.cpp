eeefbcda0c6a118098b8cf64915efce2e1c53407
Firoz kumar <56572016+Firoz-kumar-3@users.noreply.github.com>
2020-03-25 23:55:36 +0530

    Create priority_queue.cpp

---
+++ priority_queue.cpp
@@ -0,0 +1,22 @@
+//heap is impelemented as a priority queue in stl;
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	priority_queue<int> pq;
+	pq.push(3);
+	pq.push(1);
+	pq.push(299);
+	pq.push(54);
+	pq.push(43);
+	pq.push(24);
+	while(!pq.empty())
+	 {
+	 	cout<<pq.top()<<",";
+	 	pq.pop();
+	 }
+	return 0;
+}
